9830607788c5e79833c9e02ea6ed01df646eb12a
Michelle Leon <mk.leon94@gmail.com>
2016-10-19 06:25:44 +0000

    wip on event spec

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -2,9 +2,18 @@
 FactoryGirl.define do
   # default event is 1 day from now with duration of an hour
   factory :event do
+    title "DogWalk repeat"
+    description  "Walking Ms Pott's Dog"
+    starttime { 1.days.from_now }
+    endtime { 1.days.from_now + 1.hour }
+    event_series
+  end
+
+  # event factory without associated posts
+  factory :event_in_series do
     title "DogWalk"
     description  "Walking Ms Pott's Dog"
-    starttime 1.days.from_now
-    endtime 1.days.from_now + 1.hour
+    starttime { 1.days.from_now }
+    endtime { 1.days.from_now + 1.hour }
   end
 end
--- spec/factories/event_series.rb
+++ spec/factories/event_series.rb
@@ -1,12 +1,28 @@
 # This will guess the EventSeries class
 FactoryGirl.define do
   # default event series is a daily event that starts at current time
-  factory :event do
+  factory :event_series do
     title "DogWalk Series"
     description  "Walking John Smiths dog"
-    starttime Time.now
-    endtime 30.minutes.from_now
+    starttime { Time.now }
+    endtime { 30.minutes.from_now }
     period "daily"
-    frequency "4"
+    frequency "5"
+    
+    factory :event_series_with_events do
+      # posts_count is declared as a transient attribute and available in
+      # attributes on the factory, as well as the callback via the evaluator
+      transient do
+        events_count 5 # default 5 events
+      end
+
+      # the after(:create) yields two values; the event instance itself and the
+      # evaluator, which stores all values from the factory, including transient
+      # attributes; `create_list`'s second argument is the number of records
+      # to create and we make sure the event is associated properly to the post
+      after(:create) do |event_series, evaluator|
+        create_list(:event_in_series, evaluator.events_count, event_series: event_series)
+      end
+    end
   end
 end
