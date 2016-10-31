647067169452e76f5476946e64b37af4e351c10b
Steven Cheng <stevencheng@berkeley.edu>
2016-10-31 16:02:54 -0700

    kalendar#index fix

--- app/controllers/kalendar_controller.rb
+++ app/controllers/kalendar_controller.rb
@@ -1,9 +1,4 @@
 class KalendarController < ApplicationController
   def index
-    if User.exists?(params[:id])
-      @user = User.find(session[:user_id])
-    else
-      @user = nil
-    end
   end
 end
