ccfc6d28b7d4452286e58fce748a6a816d8d10f2
Michelle Leon <mk.leon94@gmail.com>
2016-11-01 01:57:06 +0000

    working facebook login with fb login home page

--- app/controllers/kalendar_controller.rb
+++ app/controllers/kalendar_controller.rb
@@ -1,10 +1,5 @@
 class KalendarController < ApplicationController
   def index
-    if session[:user_id].nil?
-      puts "*********************************************************************************************"
-      
-      redirect_to '/auth/facebook'
-    end
     @user = User.find_by_uid(session[:user_id])
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create 
     @user = User.find(params[:user])
     session[:user_id] = @user.uid
-    redirect_to root_path()
+    redirect_to kalendar_path()
   end
 
   def destroy
