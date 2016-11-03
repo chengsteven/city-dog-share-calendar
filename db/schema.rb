f919aefd197918880f3245ed5feb3bea65413a41
Steven Cheng <stevencheng@berkeley.edu>
2016-11-02 20:43:56 -0700

    remove duplicate column, update .gitignore to ignore database changes

--- db/schema.rb
+++ db/schema.rb
@@ -45,13 +45,13 @@ ActiveRecord::Schema.define(version: 20161102035449) do
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
+    t.string   "first_name"
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "last_name"
-    t.string   "first_name"
     t.string   "location"
     t.string   "gender"
     t.string   "image"
