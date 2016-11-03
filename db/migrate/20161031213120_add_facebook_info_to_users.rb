e92024dbb8eeb081488d38272be5b3a116f03cd7
chengsteven <stevencheng@berkeley.edu>
2016-11-02 21:34:22 -0700

    Steven/upgrade form (#19)
    * upgraded form to have checkboxes to indicate which days to repeat the event
    
    * update .gitignore to ignore database changes

--- db/migrate/20161031213120_add_facebook_info_to_users.rb
+++ db/migrate/20161031213120_add_facebook_info_to_users.rb
@@ -1,7 +1,6 @@
 class AddFacebookInfoToUsers < ActiveRecord::Migration
 	def change
 		add_column :users, :last_name, :string
-		add_column :users, :first_name, :string
 		add_column :users, :location, :string
 		add_column :users, :gender, :string
 		add_column :users, :image, :string
