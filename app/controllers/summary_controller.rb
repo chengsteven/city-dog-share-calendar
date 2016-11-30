6e64800768944c6f68581aace68e966c9ff0d829
Michelle Leon <mk.leon94@gmail.com>
2016-11-30 03:57:17 +0000

    got the summary table working and populated with fake data

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,5 +2,7 @@ class SummaryController < ApplicationController
   before_filter :current_user
   
   def view
+    @dogs = [{ name: "Spot" }, { name: "Fluffy" }, { name: "Lily" }]
+    @total = 900
   end
 end
