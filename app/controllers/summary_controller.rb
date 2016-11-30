73bd9704667303374b1881388d4248fb390d6e5c
Michelle Leon <mk.leon94@gmail.com>
2016-11-30 05:08:01 +0000

    updated the summary view html to use dog[:price]

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,7 +2,7 @@ class SummaryController < ApplicationController
   before_filter :current_user
   
   def view
-    @dogs = [{ name: "Spot" }, { name: "Fluffy" }, { name: "Lily" }]
+    @dogs = [{ name: "Spot", price: 300}, { name: "Fluffy", price: 300 }, { name: "Lily", price: 300 }]
     @total = 900
   end
 end
